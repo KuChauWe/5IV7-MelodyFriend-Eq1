a77c430b8b07a1bec6c79989e49f5c2ecd01533d
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2021-11-06 01:24:13 -0600

    Responsive del index <<Terminado>>

--- JS/rIndice.js
+++ JS/rIndice.js
@@ -3,7 +3,7 @@ var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
 if (/android/i.test(userAgent)) { // Aqui obtengo el dispositivo
     //Inicio con el responsive de la BARRA LATERAL
-    document.querySelector('barraLateral').style.width = '346px';
+    //document.querySelector('barraLateral').style.width = '346px'; no quiero hacerlo de esta forma, me moriría de viejo antes de acabar
 }
 if (/iPhone|iPod/.test(userAgent) && !window.MSStream) {
 
