757c058386330e1c7d2d9cb5d79948b1a3c02f6c
Eduwigsh <esolisa1900@alumno.ipn.mx>
2021-11-24 18:37:56 -0600

    subida

--- src/spotify/index.js
+++ src/spotify/index.js
@@ -2,40 +2,40 @@ var SpotifyWebApi = require('spotify-web-api-node');
 var access_token;
 var refresh_token;
 var expires_in;
-const  clientId= '45a021d73c2949abbf7926a87c1a4221';
-const clientSecret='13e5e661875540ec8f87f1d60665938d';
+const clientId = '82eb501181b642be87bd95102510eb44';
+const clientSecret = '0fc89338b2cf40c18d7a6b562f4746f5';
 
 const scopes = [
-    'ugc-image-upload',
-    'user-read-playback-state',
-    'user-modify-playback-state',
-    'user-read-currently-playing',
-    'streaming',
-    'app-remote-control',
-    'user-read-email',
-    'user-read-private',
-    'playlist-read-collaborative',
-    'playlist-modify-public',
-    'playlist-read-private',
-    'playlist-modify-private',
-    'user-library-modify',
-    'user-library-read',
-    'user-top-read',
-    'user-read-playback-position',
-    'user-read-recently-played',
-    'user-follow-read',
-    'user-follow-modify'
-  ];
-  var spotifyApi = new SpotifyWebApi({
-    clientId: clientId,
-    clientSecret: clientSecret,
-    redirectUri: 'http://localhost:3000/entrada'
-  });
-  
- Login= spotifyApi.createAuthorizeURL(scopes);
+  'ugc-image-upload',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-follow-read',
+  'user-follow-modify'
+];
+var spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret,
+  redirectUri: 'https://melodyfriend.azurewebsites.net/entrada'
+});
+
+Login = spotifyApi.createAuthorizeURL(scopes);
 
 
-module.exports=spotifyApi
- module.exports.Login=Login
- 
+module.exports = spotifyApi
+module.exports.Login = Login
+
 
