e65d8cd9a98f7f777a56f806212262b6ba0092ae
Gaelfergar <gfernandezg1900@alumno.ipn.mx>
2022-04-07 19:32:58 -0500

    Administrador de roles

---
+++ adminRoles/app/src/main/java/com/example/adminroles/ListElement.java
@@ -0,0 +1,27 @@
+package com.example.adminroles;
+
+public class ListElement {
+    public String nombre;
+    public String descripcion;
+
+    public ListElement(String nombre, String descripcion){
+        this.nombre= nombre;
+        this.descripcion= descripcion;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre= nombre;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion){
+        this.descripcion= descripcion;
+    }
+}
