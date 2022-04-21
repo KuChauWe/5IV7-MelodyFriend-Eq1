8b89c5d3997ad5e24cce5f6cd636c4e4c3e0b369
KuChauWe <KuChauWe>
2022-04-21 02:33:57 -0500

    Gráficas terminadas (sin responsive ╰（‵□′）╯).

--- Usuario/JS/verEstadisticas.js
+++ Usuario/JS/verEstadisticas.js
@@ -5,92 +5,27 @@ btnToggle.addEventListener('click', function() {
     document.getElementById('contenidoGeneral').classList.toggle('active');
 })
 
-var grafica = document.getElementById("cancionesBatiz");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"], /* when cambias el valor por el nombre de las canciones/playlists xdddd:v */
-            datasets:[{
-                label: "Nombres de gente que le mueve",
-                data: [50,50,50,50,50], /* but cambias el valor de los datos por la chaviza que escucha la playlist/canción xdddddddddddddBv */ 
-                backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
-            }]
-        },
-    });
-
-var grafica = document.getElementById("playlistsBatiz");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"],
-            datasets:[{
-            label: "Nombres de gente que le mueve",
-            data: [50,50,50,50,50],
-            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
+var grafica = document.getElementById("usuActMes");
+var myPieChart = new Chart(grafica, {
+    type: "line",
+    data: {
+        labels: ["Semana 1", "Semana 2", "Semana 3", "Semana 4"], /* when cambias el valor por el nombre de las canciones/playlists xdddd:v */
+        datasets:[{
+            label: "Usuarios activos en el mes.",
+            data: [0,10,20,30], /* but cambias el valor de los datos por la chaviza que escucha la playlist/canción xdddddddddddddBv */ 
+            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)"]
         }]
     },
 });
-
-var grafica = document.getElementById("playlistsTC");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"],
-            datasets:[{
-            label: "Nombres de gente que le mueve",
-            data: [50,50,50,50,50],
-            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
-        }]
-    },
-});
-
-var grafica = document.getElementById("playlistsProgra");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"],
-            datasets:[{
-            label: "Nombres de gente que le mueve",
-            data: [50,50,50,50,50],
-            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
-        }]
-    },
-});
-
-var grafica = document.getElementById("playlistsMSA");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"],
-            datasets:[{
-            label: "Nombres de gente que le mueve",
-            data: [50,50,50,50,50],
-            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
-        }]
-    },
-});
-
-var grafica = document.getElementById("playlistsSD");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"],
-            datasets:[{
-            label: "Nombres de gente que le mueve",
-            data: [50,50,50,50,50],
-            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
-        }]
-    },
-});
-var grafica = document.getElementById("playlistsMeca");
-    var myPieChart = new Chart(grafica, {
-        type: "doughnut",
-        data: {
-            labels: ["Hugo", "Sofo", "Edu", "Gael", "Rodrigo"],
-            datasets:[{
-            label: "Nombres de gente que le mueve",
-            data: [50,50,50,50,50],
-            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)", "rgb(88, 118, 31 )"]
+var grafica = document.getElementById("usuActDia");
+var myPieChart = new Chart(grafica, {
+    type: "line",
+    data: {
+        labels: ["0hrs-6hrs", "6hrs-12hrs", "12hrs-18hrs", "18hrs-24hrs"], /* when cambias el valor por el nombre de las canciones/playlists xdddd:v */
+        datasets:[{
+            label: "Usuarios activos en el día.",
+            data: [10,15,20,15], /* but cambias el valor de los datos por la chaviza que escucha la playlist/canción xdddddddddddddBv */ 
+            backgroundColor: ["rgb(136,204,5)", "rgb(122, 179, 11 )", "rgb(111, 160, 17)", "rgb(99, 136, 28)"]
         }]
     },
 });
