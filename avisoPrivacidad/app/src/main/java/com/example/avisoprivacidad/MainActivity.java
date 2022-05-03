fb7793e9f8b3f09198953a6ce7811e50488e957a
Gaelfergar <gfernandezg1900@alumno.ipn.mx>
2022-05-02 23:45:41 -0500

    Aviso de privacidad

--- avisoPrivacidad/app/src/main/java/com/example/avisoprivacidad/MainActivity.java
+++ avisoPrivacidad/app/src/main/java/com/example/avisoprivacidad/MainActivity.java
@@ -2,22 +2,18 @@ package com.example.avisoprivacidad;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ProgressBar;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
 public class MainActivity extends AppCompatActivity {
-
-    ProgressBar progressBar;
     PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pdfView = findViewById(R.id.pdf_viewer);
-        progressBar = findViewById(R.id.progressBar);
 
-        String urlPdf = "https://www.udlap.mx/centrodeescritura/files/curriculumVitae-Ejemplos.pdf";
-        new RecibirPDFStream(pdfView,progressBar).execute(urlPdf);
+        pdfView = findViewById(R.id.pdfview);
+
+        pdfView.fromAsset("AvisoPrivacidad.pdf").load();
     }
 }
