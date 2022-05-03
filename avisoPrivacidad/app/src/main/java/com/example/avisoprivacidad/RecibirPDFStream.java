a272e521e8a58283284a2d2a1b08f904b7c41c10
Gaelfergar <gfernandezg1900@alumno.ipn.mx>
2022-05-02 22:51:07 -0500

    Prueba

---
+++ avisoPrivacidad/app/src/main/java/com/example/avisoprivacidad/RecibirPDFStream.java
@@ -0,0 +1,45 @@
+package com.example.avisoprivacidad;
+
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RecibirPDFStream extends AsyncTask<String,Void, InputStream> {
+
+    PDFView pdfView;
+    ProgressBar progressBar;
+
+    public RecibirPDFStream(PDFView pdfView, ProgressBar progressBar) {
+        this.pdfView = pdfView;
+        this.progressBar = progressBar;
+    }
+
+    @Override
+    protected InputStream doInBackground(String... strings) {
+        InputStream inputStream = null;
+        try {
+            URL url = new URL(strings[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            if (urlConnection.getResponseCode()==200){
+                inputStream = new BufferedInputStream(urlConnection.getInputStream());
+            }
+        }catch (IOException e){
+            return null;
+        }
+        return  inputStream;
+    }
+
+    @Override
+    protected void onPostExecute(InputStream inputStream) {
+        pdfView.fromStream(inputStream).load();
+        progressBar.setVisibility(View.GONE);
+    }
+}
