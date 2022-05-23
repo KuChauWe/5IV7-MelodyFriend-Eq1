b8a66b5caedb91e8fec195f15fdbfd2a6cca1daa
KuChauWe <KuChauWe>
2022-05-23 04:16:06 -0500

    Responsive Administrador COMPLETAMENTE terminado.

--- Administrador/JS/administraRoles.js
+++ Administrador/JS/administraRoles.js
@@ -1,13 +1,12 @@
 const btnToggle = document.querySelector('.toggle-btn')
-const btnFiltro = document.querySelector('.bi-filter-circle')
 
 btnToggle.addEventListener('click', function() {
     document.getElementById('barraLateral').classList.toggle('active');
     document.getElementById('contenidoGeneral').classList.toggle('active');
 })
 
-
-var btnAbrirPopup = document.getElementsByClassName('btn-abrir-popup')[0],
+/*
+var btnAbrirPopup = document.querySelector('button'),
 	overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
@@ -18,7 +17,8 @@ btnAbrirPopup.addEventListener('click', function(){
 });
 
 btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
+	e.preventDefault();//Para q no se envíe el formulario
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
 });
+*/
